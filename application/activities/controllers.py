c6d38d8c38e624382201d3d6ed1320142bf758df
pockche123 <130081044+pockche123@users.noreply.github.com>
2024-01-10 15:11:15 +0000

    complete activity

---
+++ application/activities/controllers.py
@@ -0,0 +1,75 @@
+from flask import jsonify, request
+from werkzeug import exceptions
+from .model import Activity
+
+from .. import db
+
+
+def index(): 
+    activities = Activity.query.all()
+
+    try:
+        return jsonify({"all activities": [a.json for a in activities]})
+    except:
+        raise exceptions.InternalServerError(
+            f"Server is down. We are fixing it")
+
+
+def show(id):
+    print("Activity id: ", type(id))
+    activity = Activity.query.filter_by(activity_id=id).first()
+    try:
+        return jsonify([activity.json]), 200
+    except:
+        raise exceptions.NotFound(f"Activity not found")
+    
+
+def create():
+    try:
+        data = request.json
+
+   
+        name = data.get('name')
+        location = data.get('location')
+        filters = data.get('filters', [])
+        place_id = data.get('place_id')
+        description = data.get('description')
+        zip_code = data.get('zip_code')
+
+
+        new_activity = Activity(
+            name=name,
+            location=location,
+            filters=filters,
+            place_id=place_id,
+            description=description,
+            zip_code=zip_code
+        )
+
+        db.session.add(new_activity)
+        db.session.commit()
+
+        return jsonify(new_activity.json), 201
+
+    except Exception as e:
+        print(str(e))
+        return jsonify({"error": "Error: Activity cannot be posted"}), 400
+
+
+def update(id):
+    data = request.json
+    activity = Activity.query.filter_by(activity_id=id).first()
+
+    for (attribute, value) in data.items():
+        if hasattr(activity, attribute):
+            setattr(activity, attribute, value)
+
+    db.session.commit()
+    return jsonify({"updated activity": activity.json})
+
+
+def destroy(id):
+    activity = Activity.query.filter_by(activity_id=id).first()
+    db.session.delete(activity)
+    db.session.commit()
+    return "Activity Deleted", 204
--- application/activities/routes.py
+++ application/activities/routes.py
@@ -1,4 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from .controllers import index, show, create, update, destroy
 
 
 activities_bp = Blueprint("activities",__name__)
+
+
+@activities_bp.route('/activities', methods=["GET", "POST"])
+def handle_activities():
+    if request.method == "GET": return index()
+    if request.method == 'POST': return create()
+
+
+@activities_bp.route('/activities/<int:id>', methods=["GET", "PATCH", "DELETE"])
+def handle_character(id):
+    if request.method == "GET": return show(id)
+    if request.method == "PATCH": return update(id)
+    if request.method == "DELETE": return destroy(id)
+    
+    
