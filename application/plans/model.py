cf231255b3223d68c63c7fc9cd9346befae38422
pockche123 <130081044+pockche123@users.noreply.github.com>
2024-01-15 12:10:43 +0000

    crete and update add

--- application/plans/model.py
+++ application/plans/model.py
@@ -2,6 +2,15 @@ from application import db
 from datetime import datetime
 from application.enums import Status
 
+
+plans_activities_association = db.Table(
+    'plans_activities_association',
+    db.Model.metadata,
+    db.Column('plan_id', db.Integer, db.ForeignKey('plans.plan_id')),
+    db.Column('activity_id', db.Integer,
+    db.ForeignKey('activities.activity_id'))
+)
+
 class Plan(db.Model):
     __tablename__ = "plans"
 
@@ -10,26 +19,28 @@ class Plan(db.Model):
     guide_id = db.Column(db.Integer, db.ForeignKey('guides.guide_id'), nullable=False)
     date_from = db.Column(db.DateTime, nullable=False, default=datetime.now())
     date_to = db.Column(db.DateTime, nullable=False, default=datetime.now())
-    activity_id = db.Column(db.Integer, db.ForeignKey('activities.activity_id'), nullable=False)
+    activities = db.relationship('Activity', secondary=plans_activities_association, backref='plans', lazy='dynamic')
+    notes = db.Column(db.String())
     status = db.Column(db.Enum(Status), nullable=False)
 
-    def __init__(self, tourist_id, guide_id, date_from, date_to, activity_id, status):
+    def __init__(self, tourist_id, guide_id, date_from, date_to, status, notes):
         self.tourist_id = tourist_id
         self.guide_id = guide_id
-        self.date_to: date_to
+        self.date_to = date_to
         self.date_from = date_from
-        self.activity_id= activity_id
         self.status = status
-
-
+        self.notes = notes
+        
     @property
     def json(self):
         return {
             "plan_id": self.plan_id,
             "tourist_id": self.tourist_id,
             "guide_id": self.guide_id,
-            "date_from": self.date_from,
-            "date_to": self.date_to,
-            "activity_id": self.activity_id,
-            "status": self.status.value
+            "date_from": self.date_from.strftime("%Y-%m-%d %H:%M:%S"),
+            "date_to": self.date_to.strftime("%Y-%m-%d %H:%M:%S"),
+            "activities": [{"activity_id": activity.activity_id} for activity in self.activities],
+            "status": self.status.value,
+            "notes": self.notes
         }
+
